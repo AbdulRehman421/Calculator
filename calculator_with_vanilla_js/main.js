81fa13d9d6502b761e46b5233e81408f641933de
22_Abdul Rehman Khan <karzbaba47@gmail.com>
2022-09-10 23:59:44 +0530

    Calculator with vanilla javascript

---
+++ calculator_with_vanilla_js/main.js
@@ -0,0 +1,44 @@
+
+
+function button(a) {
+	document.getElementById("display").value += a;
+}
+
+
+
+function buttonclear() {
+	document.getElementById('display').value = " ";
+}
+
+
+function calculate() {
+	var temp = [];
+	var answer;
+
+	if (document.getElementById("display").value.includes("+")) {
+		temp = document.getElementById("display").value.split('+');
+		return answer = parseFloat(temp[0]) + parseFloat(temp[1]);
+	}
+
+	else if (document.getElementById("display").value.includes("-")) {
+		temp = document.getElementById("display").value.split('-');
+		return answer = parseFloat(temp[0]) - parseFloat(temp[1]);
+	}
+
+	else if (document.getElementById("display").value.includes("/")) {
+		temp = document.getElementById("display").value.split('/');
+		return answer = parseFloat(temp[0]) / parseFloat(temp[1]);
+	}
+
+	else {
+		temp = document.getElementById("display").value.split('*');
+		return answer = parseFloat(temp[0]) * parseFloat(temp[1]);
+	}
+}
+
+function sol() {
+	var a = calculate()
+	document.getElementById('display').value = a;
+}
+
+
